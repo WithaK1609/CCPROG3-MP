cf9f2d28546f8a9868d40add17851d9c24f25bb1
WithaK1609 <hernandez.jakob@gmail.com>
2024-07-26 03:15:24 +0800

    Updated Rooms

--- src/Rooms/ExecutiveRoom.java
+++ src/Rooms/ExecutiveRoom.java
@@ -1,15 +1,30 @@
+/**
+ * Child of Room class.
+ *
+ * <p> This is a version of the Room class that is an Executive Room.
+ * @version 1.0
+ */
 public class ExecutiveRoom extends Room{
+    /**
+     * Constructor for the Executive Room.
+     * @param name
+     */
+    
     public ExecutiveRoom(String name){
-        super(name);
-        this.description = "This is a Executive Room";
-        this.price = RoomPrice.calculateExecutivePrice(1299.99);
-        this.capacity = 6;
+        super(name, "This is a Executive Room", 6, RoomPricer.ExecutivePrice(1299.99));
     }
 
+    /**
+     * Constructor for the Executive Room.
+     * @param name
+     * @param price
+     */
     public ExecutiveRoom(String name, int price){
-        super(name);
-        this.description = "This is a Executive Room";
-        this.price = RoomPrice.calculateExecutivePrice(price);
-        this.capacity = 6;
+        super(name, "This is a Executive Room", 6, RoomPricer.ExecutivePrice(price));
+    }
+
+    @Override
+    public String toString() {
+        return "Room Name: " + this.name + "\n" + "Description: " + this.description + "\n" + "Price: " + this.price + "\n" + "Capacity: " + this.capacity + "\n";
     }
 }
