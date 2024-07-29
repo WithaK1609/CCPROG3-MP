8aae8fcc9c3fe1e02c91e3e70bcd6421951b073c
WithaK1609 <hernandez.jakob@gmail.com>
2024-07-30 01:56:06 +0800

    Revised to return boolean

--- src/main/InputLogic.java
+++ src/main/InputLogic.java
@@ -12,50 +12,49 @@ public class InputLogic{
     
     
     /**
-      * Reads a double value from the console within a specified range.
+      * Validates a double value within a specified range.
       * 
       * @param inputTextField the input entered by the user from the GUI
       * @param minChoice the minimum allowed value
       * @param maxChoice the maximum allowed value
       * @return the valid double value entered by the user
       */
-     public static double readDouble(String inputTextField, double minChoice, double maxChoice) {
+     public static boolean validateDouble(String inputTextField, double minChoice, double maxChoice) {
         double input;
         
         try{
             input = Double.parseDouble(inputTextField);
             if (input < minChoice || input > maxChoice) {
-                return -1.0;      // if input is not within the range of min and max values
+                return false;      // if input is not within the range of min and max values
             }
         }catch(NumberFormatException e) {
-            return -2.0;      // if input is not a double  
+            return false;      // if input is not a double  
         }
 
-        return input;
+        return true;
     }
 
     /** 
-     * This method reads an int from the user. 
-     * It will only accept a number between minChoice and maxChoices.
+     * Validates a double value within a specified range.
      * @param inputTextField the input entered by the user from the GUI
      * @param minChoice the minimum allowed value
      * @param maxChoices the maximum allowed value
      * @return int
      */
     
-     public static int readInt(String inputTextField, int minChoice, int maxChoices){
+     public static boolean validateInt(String inputTextField, int minChoice, int maxChoices){
 
         int input;
 
         try{
             input = Integer.parseInt(inputTextField);
             if (input < minChoice || input > maxChoices) {
-                return -1;      // if input is not within the range of min and max values
+                return false;      // if input is not within the range of min and max values
             }
         }catch(NumberFormatException e) {
-            return -2;      // if input is not an integer  
+            return false;      // if input is not an integer  
         }
         
-        return input;
+        return true;
     }
 }   
