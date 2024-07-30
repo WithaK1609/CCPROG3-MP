df3a066d6dbd11369e173061a066352b5ae36be4
WithaK1609 <hernandez.jakob@gmail.com>
2024-07-30 18:09:20 +0800

    Driver class

---
+++ src/GUI/RunGUI.java
@@ -0,0 +1,9 @@
+package GUI;
+
+public class RunGUI {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        //InputLogicGUI inputLogicGUI = new InputLogicGUI("Check: ", 0, 10);
+        
+    }
+}
